97292a646699bda785e460a0f0eb1323a65f9062
Anastasia Tetyueva <anastasia.tetyueva@me.com>
2019-04-13 21:18:30 +0200

    исправила ошибки и добавила игру НОД

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-1. Eslint
-[![Maintainability Badge](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
+1. Codeclimate
+[![Maintainability](https://api.codeclimate.com/v1/badges/51888340d338d68aff61/maintainability)](https://codeclimate.com/github/AnastasiaTetyueva/project-lvl1-s478/maintainability)
 
-2. Eslint 
-[![Test Coverage Badge](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/codeclimate/codeclimate/test_coverage)
+2. Codeclimate 
+[![Test Coverage](https://api.codeclimate.com/v1/badges/51888340d338d68aff61/test_coverage)](https://codeclimate.com/github/AnastasiaTetyueva/project-lvl1-s478/test_coverage)
 
 3. Travis
 [![Badge](https://travis-ci.org/AnastasiaTetyueva/project-lvl1-s478.svg?branch=master)](https://travis-ci.org/AnastasiaTetyueva/project-lvl1-s478)
@@ -12,3 +12,6 @@
 
 5. Пример игры "Калькулятор"
 [![asciicast](https://asciinema.org/a/j6u1YRUaOAZfUTIMo6kiJDApu.svg)](https://asciinema.org/a/j6u1YRUaOAZfUTIMo6kiJDApu)
+
+6. Пример игры "НОД"
+[![asciicast](https://asciinema.org/a/B0XVClUyryfSbeJxtRf2a5Yc3.svg)](https://asciinema.org/a/B0XVClUyryfSbeJxtRf2a5Yc3)
--- src/index.js
+++ src/index.js
@@ -1,79 +1,123 @@
 import readlineSync from 'readline-sync';
 
-export const askName = (task) => {
-    console.log(`Welcome to the Brain Games!`);
-    if (task) {
-        console.log(task);
-    }
+export const askName = () => {
     const name = readlineSync.question(`May I have your name? `);
     console.log(`Hello, ${name}!`);
     return name;
 }
 
-const game = (name, gameStep) => {
+const randomNumberCeil = (n) => {
+    return Math.ceil(n * Math.random());
+}
+const randomNumberFloor = (n) => {
+    return Math.floor(n * Math.random());
+}
+
+const game = (task, gameStep) => {
+    console.log(`Welcome to the Brain Games!`);
+    console.log(task);
+    let name = askName();
     const numberOfTries = 3;
-    for(let i = 0; i < numberOfTries; i++) {
-        if(!gameStep()) {
+    for (let i = 0; i < numberOfTries; i++) {
+        if (!gameStep()) {
             console.log(`Let is try again, ${name}!`);
             return;
         }
-        console.log(`Congratulations, ${name}!`);
     }
+    console.log(`Congratulations, ${name}!`);
 }
 
 const isEven = (number) => {
-    if(number % 2 == 0) {
+    if (number % 2 == 0) {
         return true;
     }
     return false;
 }
 
 const evenGameStep = () => {
-    let randomNumber = Math.ceil(100 * Math.random());
-    console.log(`Question: ${randomNumber}`);
+    let range = 100;
+    let number = randomNumberCeil(range);
+    console.log(`Question: ${number}`);
     const answer = readlineSync.question(`Your answer: `).toLowerCase();
-    if(isEven(randomNumber) == (answer == 'yes')){
+    if (isEven(number) == (answer == 'yes')){
         console.log(`Correct!`);
         return true;
     }
     else {
-        console.log(`"${answer}" is wrong answer ;(. Correct answer was "${isEven(randomNumber) ? "yes" : "no"}"`);
+        console.log(`"${answer}" is wrong answer ;(. Correct answer was "${isEven(number) ? "yes" : "no"}"`);
         return false;
-        }
+    }
 }
 
 export const checkEven = () => {
-    let name = askName(`Answer "yes" if number even otherwise answer "no".`);
-    game(name, evenGameStep);
+    game(`Answer "yes" if number even otherwise answer "no".`, evenGameStep);
 }
 
 export const calc = () => {
-    let name = askName(`What is the result of the expression?`);
-    game(name, calcGameStep);
+    game(`What is the result of the expression?`, calcGameStep);
 }
 
 const calcGameStep = () => {
-    let number1 = Math.ceil(100 * Math.random());
-    let number2 = Math.ceil(100 * Math.random());
-    let operation = Math.floor(3 * Math.random());
-    console.log(`Question: ${number1} ${["+","-","*"][operation]} ${number2}`);
+    const operators = ["+","-","*"];
+    const range = 100;
+    let number1 = randomNumberCeil(range); 
+    let number2 = randomNumberCeil(range); 
+    let operation = randomNumberFloor(operators.length);
+    console.log(`Question: ${number1} ${operators[operation]} ${number2}`);
     const answer = parseInt(readlineSync.question(`Your answer: `));
     let decision = 0;
-    if(operation == 0) {
+    switch (operation) {
+        case 0:
         decision = number1 + number2;
-    }
-    if(operation == 1) {
+        break;
+        case 1:
         decision = number1 - number2;
-    }
-    if(operation == 2) {
+        break;
+        case 2:
         decision = number1 * number2;
+        break;
     }
-    if(decision == answer){
+    if (decision == answer) {
         console.log(`Correct!`);
         return true;
     }
     else {
         console.log(`"${answer}" is wrong answer ;(. Correct answer was ${decision}`);
         return false;
-        }
+    }
 }
+
+export const findGcd = () => {
+    game(`Find the greatest common divisor of given numbers.`, findGcdGameStep);
+}
+
+const gcd = (a,b) => {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {
+        let temp = a; a = b; b = temp;
+    }
+    while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a;
+    }
+}
+
+const findGcdGameStep = () => {
+    let range = 100;
+    let number1 = randomNumberCeil(range); 
+    let number2 = randomNumberCeil(range); 
+    console.log(`Question: ${number1} ${number2}`);
+    const answer = parseInt(readlineSync.question(`Your answer: `));
+    if (gcd(number1, number2) == answer){
+        console.log(`Correct!`);
+        return true;
+    }
+    else {
+        console.log(`"${answer}" is wrong answer ;(. Correct answer was ${gcd(number1, number2)}`);
+        return false;
+    }
+}
+
